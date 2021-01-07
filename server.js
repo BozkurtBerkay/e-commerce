f1850f5c6024032b5aaf710539217a50502bb0b7
Berkay Bozkurt <BozkurtBerkay@hotmail.com>
2021-01-07 20:29:17 +0300

    hadi be caliscan mi

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ mongoose.connect(URI,{
     if(err) throw err;
     console.log('MongoDB Bağlandı...')
 })
- 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {

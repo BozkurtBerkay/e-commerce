90ee955670bdcd3097c2a0047f92c47d70395cbb
Berkay Bozkurt <BozkurtBerkay@hotmail.com>
2021-01-07 20:37:57 +0300

    oh be calisti

--- client/src/components/mainpages/products/Products.js
+++ client/src/components/mainpages/products/Products.js
@@ -64,9 +64,9 @@ function Products() {
         {
             isAdmin && 
             <div className="delete-all">
-                <span>Select all</span>
+                <span>Hepsini seç</span>
                 <input type="checkbox" checked={isCheck} onChange={checkAll} />
-                <button onClick={deleteAll}>Delete ALL</button>
+                <button onClick={deleteAll}>Hepsini Sil</button>
             </div>
         }
 
